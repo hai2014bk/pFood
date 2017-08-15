797ecd221f1a31ff4331ffaf6e9622b5521fedbf
truong son <sonnguyen25105@gmai.com>
2017-08-15 18:23:44 +0700

    register screen

--- js/components/sign-up/styles.js
+++ js/components/sign-up/styles.js
@@ -8,51 +8,38 @@ const deviceWidth = Dimensions.get("window").width;
 const primary = require("../../themes/variable").brandPrimary;
 
 export default {
-	signupContainer: {
-		paddingLeft: 20,
-		paddingRight: 20,
-		marginTop:
-			deviceWidth < 330
-				? Platform.OS === "android" ? deviceHeight / 9 - 10 : deviceHeight / 9 - 10
-				: Platform.OS === "android" ? deviceHeight / 7 - 10 : deviceHeight / 5 - 10,
-	},
-	signupHeader: {
-		alignSelf: "center",
-		fontSize: 22,
-		padding: 10,
-		fontWeight: "bold",
-		color: "#FFF",
-		marginTop: Platform.OS === "android" ? deviceHeight / 6 : deviceHeight / 6 + 10,
-	},
-	background: {
+	container: {
 		flex: 1,
-		width: null,
-		height: null,
-		backgroundColor: primary,
+		padding: 20,
+		backgroundColor: 'white',
 	},
-	inputGrp: {
-		flexDirection: "row",
-		borderRadius: 25,
-		backgroundColor: "rgba(255,255,255,0.2)",
-		marginBottom: 20,
-		borderWidth: 0,
-		borderColor: "transparent",
+	showPassword: {
+		color: 'black',
+		marginLeft: 20
 	},
-	input: {
-		paddingLeft: 10,
-		color: "#FFF",
+	button: {
+		height: 30,
+		marginTop: 20
 	},
-	signupBtn: {
-		height: 50,
-		marginTop: 20,
-		borderWidth: 1,
-		borderColor: "#ddd",
+	questionText: {
+		color: 'black',
+		fontSize: 12
 	},
-	termsText: {
-		alignSelf: "center",
-		opacity: 0.8,
-		fontSize: 14,
-		fontWeight: "bold",
-		color: "#EFF",
+	questionWrap:{
+		marginTop: 20
 	},
+	col: {
+		justifyContent: 'flex-end',
+		alignItems: 'center'
+	},
+	checkBoxWrap: {
+		marginTop: 15,
+		paddingLeft: 10
+	},
+	line: {
+		height: 3,
+		width: '100%',
+		backgroundColor: '#cecece'
+	}
+
 };
