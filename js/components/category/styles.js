18d1556d59ddb3898aef8dcd153a5ed2558832fb
truong son <sonnguyen25105@gmai.com>
2017-08-18 18:11:46 +0700

    fix category

--- js/components/category/styles.js
+++ js/components/category/styles.js
@@ -6,24 +6,26 @@ const deviceHeight = Dimensions.get("window").height;
 export default {
     container: {
         flex: 1,
-        backgroundColor:'white'
+        backgroundColor:'white',
     },
     content: {
-        flex: 1
+        flex: 1,
     },
     imageWrap: {
         marginLeft: -10,
-        marginRight: 10
+        marginRight: 10,
     },
     image:{
-        height:90,
-        width: 90,
+        height:80,
+        width: 80,
+        borderRadius:15
     },
     infoWrap:{
         flexDirection:'column',
         alignItems:'flex-start',
-        flex:1,
-        justifyContent:'center'
+        flex:1.5,
+        justifyContent:'center',
+        marginLeft:15
     },
     info:{
         color:'black',
@@ -32,15 +34,28 @@ export default {
     buttonWrap:{
         flexDirection:'row',
         flex:1.5,
-        marginLeft:10
+        marginLeft:10,
+        justifyContent:'center',
+        paddingRight:20,
+        marginTop:20
     },
     quantity:{
-        marginTop:12,
-        marginLeft:5
+        marginTop:3,
+        marginRight:5,
+        marginLeft:2
     },
     cartWrap:{
         flex:1,
         alignItems:'flex-end',
         marginLeft:5
+    },
+    iconWrap:{
+        height:30,
+        width:30,
+        marginLeft:10
+    },
+    icon:{
+        fontSize: 25,
+        color:'red'
     }
 };
