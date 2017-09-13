adf524dbd12928eed270d246df9927b4a3090c5d
truong son <sonnguyen25105@gmail.com>
2017-09-13 14:55:19 +0700

    fetch purveyor

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -14,6 +14,7 @@ import { addOrder } from "./addOrder"
 import { reRender } from "./header"
 import { fetchStoresDetail, fetchStoreProduct, fetchStoreBanner } from "./fetchStoresDetail"
 import { fetchBannerRecomend, fetchTrendingRecomend, fetchLastestRecomend, fetchLowestRecomend } from "./fetchTrendingRecomend"
+import { fetchPurveyor } from "./fetchPurveyor"
 
 
 
@@ -39,6 +40,7 @@ export default combineReducers({
 	fetchLastestRecomend,
 	fetchLowestRecomend,
 	fetchBannerRecomend,
+	fetchPurveyor,
 	reRender,
 
 
