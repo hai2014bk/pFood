292359d95b1700fb176d9f1f267b82835f9b51c9
DESKTOP-S8LO71A\doant <doanthanh58863@gmail.com>
2017-09-20 16:48:17 +0700

    add purveyor into search store

--- js/components/searchStore/index.js
+++ js/components/searchStore/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import {ActivityIndicator, Dimensions, Alert, FlatList, InteractionManager, AsyncStorage, Text, Image, View, TouchableOpacity } from "react-native";
+import { ActivityIndicator, Dimensions, Alert, FlatList, InteractionManager, AsyncStorage, Text, Image, View, TouchableOpacity } from "react-native";
 import * as mConstants from '../../utils/Constants'
 import StarRating from 'react-native-star-rating';
-import {Button,Input,Item, Icon, List, ListItem, Header, Container, Content, Thumbnail } from "native-base";
+import { Button, Input, Item, Icon, List, ListItem, Header, Container, Content, Thumbnail } from "native-base";
 import { Grid, Col, Row } from "react-native-easy-grid";
 import HeaderContent from "./../headerContent/";
 import Swiper from 'react-native-swiper';
@@ -10,6 +10,7 @@ import { connect } from "react-redux";
 import styles from "./styles";
 import { fetchStores } from "../../actions/fetchStores.js"
 import { fetchBanner } from "../../actions/fetchStoresDetail.js"
+import { fetchPurveyor } from "../../actions/fetchPurveyor.js"
 import Spinner from 'react-native-loading-spinner-overlay';
 import Communications from 'react-native-communications';
 import Carousel from 'react-native-banner-carousel';
@@ -24,11 +25,13 @@ class SearchStore extends Component {
             data: [],
             disabled: false,
             banners: [],
-            isLoading: false
+            isLoading: false,
+            searchClick: false,
+            dataPurveyor: [],
         };
     }
     componentDidMount() {
-       
+
     }
 
     componentWillReceiveProps(props) {
@@ -36,8 +39,13 @@ class SearchStore extends Component {
         this.setState({ isLoading: false })
         if (props.fetchStores.success) {
             this.setState({ data: props.fetchStores.data.model, isLoading: false })
+            console.log('start data', this.state.data)
         }
-        if (!props.fetchStores.success) {
+        if (props.fetchPurveyor.success) {
+            this.setState({ dataPurveyor: props.fetchPurveyor.data.model, isLoading: false })
+            console.log('start data', this.state.dataPurveyor)
+        }
+        if (!props.fetchStores.success && !props.fetchPurveyor.success) {
             this.setState({ isLoading: false })
             setTimeout(() => { Alert.alert('Lỗi mạng', 'Có vấn đề khi kết nối đến máy chủ') }, 200)
         }
@@ -70,9 +78,9 @@ class SearchStore extends Component {
             )
         }
         return (
-            <View style={{ flex:1, height:137 }} style={styles.slide1}>
-                    <ActivityIndicator style={{height:137}}/>
-                </View>
+            <View style={{ flex: 1, height: 137 }} style={styles.slide1}>
+                <ActivityIndicator style={{ height: 137 }} />
+            </View>
         )
     }
     renderStar(rate) {
@@ -99,6 +107,76 @@ class SearchStore extends Component {
             this.setState({ disabled: false })
         })
     }
+
+    renderList(data, key) {
+        if (this.state.searchClick == true) {
+            if (key === 'store') {
+                if (!data[0]) {
+                    return (
+                        <View style={styles.bodyWrap}>
+                            <View style={styles.titleWrap}>
+                                <Text style={styles.titleText}>Cửa hàng</Text>
+                            </View>
+                            <Text style={styles.noDataText}>Không tìm thấy cửa hàng phù hợp với yêu cầu tìm kiếm</Text>
+                        </View>
+                    )
+                } else {
+                    return (
+                        <View style={styles.bodyWrap}>
+                            <View style={styles.titleWrap}>
+                                <Text style={styles.titleText}>Cửa hàng</Text>
+                            </View>
+                            <FlatList style={{}}
+                                data={this.state.data}
+                                extraData={this.state.data}
+                                keyExtractor={this._keyExtractor}
+                                numColumns={2}
+                                renderItem={(item) => (
+                                    <View style={styles.listItemWrap} >
+                                        {this.renderStoreList(item)}
+                                    </View>
+                                )
+                                }
+                            />
+                        </View>
+                    )
+                }
+            }
+            if (key = 'purveyor') {
+                if (!data[0]) {
+                    return (
+                        <View style={styles.bodyWrap}>
+                            <View style={styles.titleWrap}>
+                                <Text style={styles.titleText}>Trang trại</Text>
+                            </View>
+                            <Text style={styles.noDataText}>Không tìm thấy trang trại phù hợp với yêu cầu tìm kiếm</Text>
+                        </View>
+                    )
+                } else {
+                    return (
+                        <View style={styles.bodyWrap}>
+                            <View style={styles.titleWrap}>
+                                <Text style={styles.titleText}>Trang trại</Text>
+                            </View>
+                            <FlatList style={{}}
+                                data={this.state.dataPurveyor}
+                                extraData={this.state.dataPurveyor}
+                                keyExtractor={this._keyExtractor}
+                                numColumns={2}
+                                renderItem={(item) => (
+                                    <View style={styles.listItemWrap} >
+                                        {this.renderStoreList(item)}
+                                    </View>
+                                )
+                                }
+                            />
+                        </View>
+                    )
+                }
+            }
+        }
+    }
+
     renderStoreList(data) {
         var item = data.item
         console.log(item)
@@ -117,60 +195,45 @@ class SearchStore extends Component {
                             <Icon name='ios-call' style={styles.phoneIcon} />
                             <Text onPress={() => Communications.phonecall('0987678911', true)} style={styles.hotline}>0987678911</Text>
                         </View>
-                        <Text style={styles.address}>{item.hqAddress}</Text>
+                        <Text style={styles.address}>{item.address}</Text>
                     </View>
                 </View>
             </TouchableOpacity>
         )
     }
     _keyExtractor = (item, index) => item.id;
-    search(){
+    search() {
         let params = {}
+        let purveyParams = {}
         params.city = 'Hanoi'
         params.pageSize = 100
         params.pageIndex = 1
-        this.setState({isLoading:true})
+        params.searchTerm = ''
+        this.setState({ isLoading: true })
         this.props.fetch(params)
+        this.props.fetchPurvey(params)
+        this.setState({ searchClick: true })
     }
-    deleteSearch(){
-		this.setState({searchText:''})
-	}
-    render() {
+    deleteSearch() {
+        this.setState({ searchText: '' })
+    }
+    render() {        
         const navigation = this.props.screenProps.navi;
+        const { params } = this.props.navigation.state
         console.log(this.state.disabled)
         return (
             <Container style={styles.container}>
-                	<Header searchBar rounded>
-					<Item>
-						<Icon name="ios-search" />
-						<Input value={this.state.searchText} 
-						onChangeText={(text)=> {this.setState({searchText:text})}} 
-						ref={(search) => { this.searchInput = search }} 
-						placeholder="Tìm kiếm" />
-						<TouchableOpacity onPress={()=>{this.deleteSearch()}}>
-							<Icon name="ios-close-circle" />
-						</TouchableOpacity>
-					</Item>
-					<Button transparent onPress={()=>{this.search()}}>
-						<Text style={{color:'white'}}>Tìm kiếm</Text>
-					</Button>
-				</Header>
+                <HeaderContent keyboardShouldPersistTaps='handle'
+                    navi={navigation}
+                    value={this.state.searchText}
+                    changeText={(text) => { this.setState({ searchText: text }) }}
+                    leftSearchMenu={() => navigation.navigate('DrawerOpen')} rightButton={true} leftSearchMenuIcon={'menu'}
+                    deleteSearch={() => { this.deleteSearch() }}
+                    search={() => this.search()} />
                 <Content>
                     <Spinner visible={this.state.isLoading} />
-                    <View style={styles.bodyWrap}>
-                        <FlatList style={{}}
-                            data={this.state.data}
-                            extraData={this.state.data}
-                            keyExtractor={this._keyExtractor}
-                            numColumns={2}
-                            renderItem={(item) => (
-                                <View style={styles.listItemWrap} >
-                                    {this.renderStoreList(item)}
-                                </View>
-                            )
-                            }
-                        />
-                    </View>
+                    {this.renderList(this.state.data, 'store')}
+                    {this.renderList(this.state.dataPurveyor, 'purveyor')}
                 </Content>
             </Container>
         );
@@ -179,13 +242,15 @@ class SearchStore extends Component {
 function bindActions(dispatch) {
     return {
         fetch: (params) => dispatch(fetchStores(params)),
-        fetchBanner: () => dispatch(fetchBanner())
+        fetchBanner: () => dispatch(fetchBanner()),
+        fetchPurvey: (params) => dispatch(fetchPurveyor(params))
     };
 }
 
 const mapStateToProps = state => ({
     fetchStores: state.fetchStores,
-    fetchStoreBanner: state.fetchStoreBanner
+    fetchStoreBanner: state.fetchStoreBanner,
+    fetchPurveyor: state.fetchPurveyor
 });
 
 export default connect(mapStateToProps, bindActions)(SearchStore);
